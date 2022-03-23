829edc3362a584ac29cdb43d1de5759cc5e2fe00
Dean Kou <kou@penseur.co.jp>
2022-03-24 01:17:23 +0900

    update home page

---
+++ components/Top.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="l-wrap">
+    <h1 class="u-text-3xl u-font-bold u-underline sp:u-text-green-500">
+      Hello world!
+    </h1>
+    <img id="sakura" src="~/assets/img/sakura.jpg">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Top'
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <Tutorial/>
+  <Top/>
 </template>
 
 <script>
