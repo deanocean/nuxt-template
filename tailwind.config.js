1cb1c651568b0cb15ded72a2f1f3f440f22aab98
Dean Kou <kou@penseur.co.jp>
2022-03-24 00:52:34 +0900

    add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    screens: {
+      'sp': {'max': '768px'}
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  prefix: 'u-',
+}
+
