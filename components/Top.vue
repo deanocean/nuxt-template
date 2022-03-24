3dde90be77a6f8ad218d08ffe32ff0d2983546d4
Dean Kou <kou@penseur.co.jp>
2022-03-24 21:38:42 +0900

    update Top.vue

--- components/Top.vue
+++ components/Top.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="l-wrap">
-    <h1 class="u-text-3xl u-font-bold u-underline sp:u-text-green-500">
+    <h1 class="text-3xl !font-bold u-underline sp:u-text-green-500">
       Hello world!
     </h1>
     <img id="sakura" src="~/assets/img/sakura.jpg">
+    <Button 
+      theme="primary"
+      label="Click here"
+      hover
+      hoverType="Re"
+      extraClassName="u-mt-20 u-mb-20"
+    />
   </div>
 </template>
 
