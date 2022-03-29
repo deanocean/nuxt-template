4bf5c1bbf83b220e807214cc161ea45c2f3d070f
Dean Kou <kou@penseur.co.jp>
2022-03-29 20:03:39 +0900

    upgrade @nuxtjs/tailwindcss to ^5.0 to fix Important modifier bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 module.exports = {
-  darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
       'sp': {'max': '768px'}
     }
   },
   prefix: 'u-',
-  important: true,
+  // important: true,
 }
 
